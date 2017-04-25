6801f7dcd6de2d0b7e60e505244a33e0e2c2b4ab
kwon <kwon.youngjae@sbsnt.co.jp>
2017-04-25 18:48:46 +0900

    change dir

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ require('font-awesome/css/font-awesome.css');
 // index.html 를 require 하여 dist 에 복사 되도록 한다.
 require('./index.html');
 
-var Elm = require('./Main.elm');
+var Elm = require('./elm/Main.elm');
 var mountNode = document.getElementById('main');
 
 // .embed() 는 두번째 인자를 받을 수 있다. 이는 프로그램에 필요한 데이터가 될 수 있다. (예: userID, 토큰 등)
