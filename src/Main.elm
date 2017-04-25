20eaeaf3a129b740fde8c94bd9fc315d6e39f945
kwon <kwon.youngjae@sbsnt.co.jp>
2017-04-25 10:18:29 +0900

    show initialModel

--- src/Main.elm
+++ src/Main.elm
@@ -2,7 +2,7 @@ module Main exposing (..)
 
 import Html exposing (Html, div, text, program)
 import Messages exposing (Msg)
-import Models exposing (Model)
+import Models exposing (Model, initialModel)
 import Update exposing (update)
 import View exposing (view)
 
@@ -12,7 +12,7 @@ import View exposing (view)
 
 init : ( Model, Cmd Msg )
 init =
-    ( "Hello", Cmd.none )
+    ( initialModel, Cmd.none )
 
 
 
