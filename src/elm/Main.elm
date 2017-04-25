e5f15204f46ac5ccf3a87379bc2efb8abfb3b21a
kwon <kwon.youngjae@sbsnt.co.jp>
2017-04-25 19:21:18 +0900

    Router

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,19 +1,26 @@
 module Main exposing (..)
 
-import Html exposing (Html, div, text, program)
 import Messages exposing (Msg(..))
 import Models exposing (Model, initialModel)
+import Players.Commands exposing (fetchAll)
+import Navigation exposing (Location)
+import Routing exposing (Route)
 import Update exposing (update)
 import View exposing (view)
-import Players.Commands exposing (fetchAll)
 
 
 -- MODEL
+-- NavigationからLocationが渡される
+-- LocationをParsingしてReturnされたrouteは、modelに保存される
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( initialModel, Cmd.map PlayersMsg fetchAll )
+init : Location -> ( Model, Cmd Msg )
+init location =
+    let
+        currentRoute =
+            Routing.parseLocation location
+    in
+        ( initialModel currentRoute, Cmd.map PlayersMsg fetchAll )
 
 
 
@@ -27,11 +34,13 @@ subscriptions model =
 
 
 -- MAIN
+-- Html.programではなく、Navigation.programを使う
+-- Navigation.programは、browserの経路変更時メッセージを出す（ここではOnLocationChange）
 
 
 main : Program Never Model Msg
 main =
-    program
+    Navigation.program OnLocationChange
         { init = init
         , view = view
         , update = update
--- src/elm/Routing.elm
+++ src/elm/Routing.elm
@@ -5,10 +5,21 @@ import Players.Models exposing (PlayerId)
 import UrlParser exposing (..)
 
 
+-- navigation：browserのURL変更に対応
+-- elm package install elm-lang/navigation
+-- url-parser：Root Matcherを提供
+-- elm package install evancz/url-parser
+
+
 type Route
     = PlayersRoute
     | PlayerRoute PlayerId
     | NotFoundRoute
+
+
+
+-- Matcher（Parserは、url-parserで提供）
+-- http://package.elm-lang.org/packages/evancz/url-parser/2.0.1
 
 
 matchers : Parser (Route -> a) a
