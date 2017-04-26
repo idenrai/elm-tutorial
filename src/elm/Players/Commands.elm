f06d3690a800335bbb435dc602a7dd53909ef5c0
kwon <kwon.youngjae@sbsnt.co.jp>
2017-04-26 12:13:29 +0900

    update

--- src/elm/Players/Commands.elm
+++ src/elm/Players/Commands.elm
@@ -1,6 +1,7 @@
 module Players.Commands exposing (..)
 
 import Http
+import Json.Encode as Encode
 import Json.Decode as Decode exposing (field)
 import Players.Models exposing (PlayerId, Player)
 import Players.Messages exposing (..)
@@ -39,3 +40,40 @@ memberDecoder =
         (field "id" Decode.string)
         (field "name" Decode.string)
         (field "level" Decode.int)
+
+
+saveUrl : PlayerId -> String
+saveUrl playerId =
+    "http://localhost:4000/players/" ++ playerId
+
+
+saveRequest : Player -> Http.Request Player
+saveRequest player =
+    Http.request
+        { method = "PATCH"
+        , headers = []
+        , url = saveUrl player.id
+        , body = memberEncoded player |> Http.jsonBody -- PlayerをJSON文字列でEncode
+        , expect = Http.expectJson memberDecoder -- 答えをどうParsingするか指定（今はJSONが返ってくるとElmにDecoding）
+        , timeout = Nothing
+        , withCredentials = False
+        }
+
+
+save : Player -> Cmd Msg
+save player =
+    saveRequest player
+        |> Http.send OnSave
+
+
+memberEncoded : Player -> Encode.Value
+memberEncoded player =
+    let
+        list =
+            [ ( "id", Encode.string player.id )
+            , ( "name", Encode.string player.name )
+            , ( "level", Encode.int player.level )
+            ]
+    in
+        list
+            |> Encode.object
